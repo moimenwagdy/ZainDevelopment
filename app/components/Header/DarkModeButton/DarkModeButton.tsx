1b7a74b421b7003ce0f1fb555975dfc339fb9434
moimenwagdy <moimenwy@gmail.com>
2025-05-09 03:05:26 +0300

    created categories and product sections

--- app/components/Header/DarkModeButton/DarkModeButton.tsx
+++ app/components/Header/DarkModeButton/DarkModeButton.tsx
@@ -6,10 +6,21 @@ import ModeButton from "./ModeButton";
 export default function DarkMode() {
   const { theme, setTheme } = useTheme();
   const [mounted, setMounted] = useState(false);
-
+  const [LightDisabled, setLightDisabled] = useState<boolean>(false);
+  const [DarkDisabled, setDarkDisabled] = useState<boolean>(false);
   useEffect(() => {
     setMounted(true);
   }, []);
+
+  useEffect(() => {
+    if (theme === "dark") {
+      setLightDisabled(false);
+      setDarkDisabled(true);
+    } else {
+      setLightDisabled(true);
+      setDarkDisabled(false);
+    }
+  });
 
   if (!mounted) return null;
 
@@ -19,16 +30,8 @@ export default function DarkMode() {
 
   return (
     <div className=" font-sans text-sm z-100 gap-x-1 ring rounded dark:ring-blackColor px-1 flex">
-      <ModeButton
-        disabled={theme === "dark"}
-        moodValue="dark"
-        onClick={toggle}
-      />
-      <ModeButton
-        disabled={theme === "light"}
-        moodValue="light"
-        onClick={toggle}
-      />
+      <ModeButton disabled={DarkDisabled} moodValue="dark" onClick={toggle} />
+      <ModeButton disabled={LightDisabled} moodValue="light" onClick={toggle} />
     </div>
   );
 }
